533e99bc04dae5d748000c9034efa96f9bd4ef11
Mykola Kosholap <mykola.kosholap@gmail.com>
2016-09-23 01:24:13 +0300

    реалізована функція видалення даних, з'явилась проблема екранування тегів, якщо не перезавантажена сторінка

--- todo.js
+++ todo.js
@@ -1,25 +1,27 @@
 var list = document.querySelector("#list ol");
 var idMask = 'id_';
 getResult();
+
 function addTask() {
     var value = document.getElementById('headText').value;
     if (!value) alert("введіть текст!");
     addToList(value);
 }
-function addToList(value) {
 
+function addToList(value) {
     var id = 0;
     for(var i = 0; i < list.children.length; i++){
         var newId = list.children[i].getAttribute('data-item').slice(3);
             if(newId > id) id = newId;
-    }
-    id++;
+    }id++;
     localStorage.setItem(idMask+id,value);
     var child = document.createElement('li');
-    child.innerText = value;
+    child.innerText = '<div class="delete" id="' + idMask+id +'">X</div>' + value;
+    child.setAttribute('id', idMask+id);
     child.setAttribute('data-item', idMask+id);
     list.insertBefore(child, document.body.nextSibling);
 }
+
 function getResult() {
     var local = localStorage.length;
     if(local > 0){
@@ -27,12 +29,25 @@ function getResult() {
             var key = localStorage.key(i);
             if(key.indexOf(idMask) == 0){
                 var child = document.createElement('li');
-                child.innerText = localStorage.getItem(key);
+                child.innerHTML = '<div class="delete" id="' + key +'">X</div>' +  localStorage.getItem(key);
+                child.setAttribute('id', key);
                 child.setAttribute('data-item', key);
                 list.insertBefore(child, document.body.nextSibling);
             }
         }
     }
 }
+
+function deleteEl(){
+    var getId = this.getAttribute('id');
+    var idLi = getId + '';
+    idLi = document.getElementById(idLi);
+    localStorage.removeItem(idLi.getAttribute('data-item'));
+    idLi.remove();
+}
+var deleteDiv = document.getElementsByClassName('delete');
+    for(var i = 0; i < deleteDiv.length; i++){
+        deleteDiv[i].addEventListener('click', deleteEl);
+    }
 
 
