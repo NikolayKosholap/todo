56931dc98594e43394200ad04b48d84ac351a8f3
Mykola Kosholap <mykola.kosholap@gmail.com>
2016-09-29 21:12:14 +0300

    localStorage Object

--- todo.js
+++ todo.js
@@ -1,53 +1,60 @@
-var list = document.querySelector("#list ol");
-var idMask = 'id_';
+var list = document.querySelector('#list ol');
 getResult();
+function obj(){
+
+    var valueObj = {
+
+    };
+    var todoStr = localStorage.getItem('todo');
+    if (todoStr !== null) {
+        valueObj = JSON.parse(todoStr);    }
+    return valueObj;
+}
 
 function addTask() {
-    var value = document.getElementById('headText').value;
-    if (!value) alert("введіть текст!");
-    addToList(value);
+    var newVal = document.getElementById('headText').value;
+    if (!newVal) alert("введіть текст!");
+    var valueObj = new obj();
+    var iden = 0;
+    for(var i = 0; i < list.children.length; i++){
+        var newId = list.children[i].getAttribute('id').slice(3);
+        if(newId > iden) iden = newId;
+    }iden++;
+    valueObj[iden] = {};
+
+    valueObj[iden].title = newVal;
+    localStorage.setItem('todo', JSON.stringify(valueObj));
+    getResult();
 }
 
-function addToList(value) {
-    var id = 0;
-    for(var i = 0; i < list.children.length; i++){
-        var newId = list.children[i].getAttribute('data-item').slice(3);
-            if(newId > id) id = newId;
-    }id++;
-    localStorage.setItem(idMask+id,value);
-    var child = document.createElement('li');
-    child.innerText = '<div class="delete" id="' + idMask+id +'">X</div>' + value;
-    child.setAttribute('id', idMask+id);
-    child.setAttribute('data-item', idMask+id);
-    list.insertBefore(child, document.body.nextSibling);
-}
+
 
 function getResult() {
-    var local = localStorage.length;
-    if(local > 0){
-        for(var i = 0; i < local; i++){
-            var key = localStorage.key(i);
-            if(key.indexOf(idMask) == 0){
-                var child = document.createElement('li');
-                child.innerHTML = '<div class="delete" id="' + key +'">X</div>' +  localStorage.getItem(key);
-                child.setAttribute('id', key);
-                child.setAttribute('data-item', key);
-                list.insertBefore(child, document.body.nextSibling);
-            }
-        }
+    var valueObj = new obj();
+    for(var i = 1; i < Object.keys(valueObj).length+1; i++){
+        var child = document.createElement('li');
+        child.innerHTML = '<div id ="' + 'id_' + i + '" class="delete">X</div><input type="checkbox"/>' + valueObj[i].title;
+        child.setAttribute('id', 'id_'+i);
+        list.insertBefore(child, list.firstChild);
     }
 }
 
 function deleteEl(){
     var getId = this.getAttribute('id');
+    var valueObj = obj();
     var idLi = getId + '';
     idLi = document.getElementById(idLi);
-    localStorage.removeItem(idLi.getAttribute('data-item'));
+    var idRemove = getId.slice(3);
+    delete valueObj[idRemove];
     idLi.remove();
+    localStorage.setItem('todo', JSON.stringify(valueObj));
+
+    //return getResult();
 }
 var deleteDiv = document.getElementsByClassName('delete');
-    for(var i = 0; i < deleteDiv.length; i++){
-        deleteDiv[i].addEventListener('click', deleteEl);
-    }
+for(var i = 0; i < deleteDiv.length; i++){
+    deleteDiv[i].addEventListener('click', deleteEl);
+}
+document.getElementById('form').addEventListener('submit', addTask);
 
 
